754825bbc5b8e1e1560f9a2c4d1f74a39e23013f
PKRekha <rekha7.p@gmail.com>
2019-09-12 17:03:03 +0530

    added summary explination and little changes to the code

--- localStorageExample.js
+++ localStorageExample.js
@@ -1,5 +1,10 @@
+//Example summary:
+//Take a list of elements, and take an element from the user. Check if the element is presesnt in the list,
+//if it is then it is valid and if not add the element to the list, so that when next time user enter the same value, it has to be valid.
+
+
 var my_list=['nodeJs','Javascript','AngularJs','Angular','React'];
-var eleCheck='jQuery';
+var eleCheck=prompt('Enter any subject!!!!!');
 	for(i=0;i<my_list.length;i++){
 		if(eleCheck==my_list[i])
 			console.log('Valid subject');

76a70633646dc07f16bf75046e42b027bbed3e16
PKRekha <rekha7.p@gmail.com>
2019-09-14 19:02:09 +0530

    added looping an array using map function

--- localStorageExample.js
+++ localStorageExample.js
@@ -1,6 +1,6 @@
-//Example summary:
-//Take a list of elements, and take an element from the user. Check if the element is presesnt in the list,
-//if it is then it is valid and if not add the element to the list, so that when next time user enter the same value, it has to be valid.
+/*Example summary:
+Take a list of elements, and take an element from the user. Check if the element is presesnt in the list,
+if it is then it is valid and if not add the element to the list, so that when next time user enter the same value, it has to be valid.*/
 
 
 var my_list=['nodeJs','Javascript','AngularJs','Angular','React'];
@@ -13,3 +13,15 @@ var eleCheck=prompt('Enter any subject!!!!!');
 	}
 	my_list.push(eleCheck);
 	localStorage.setItem('myArray',my_list);
+/************************************************************/	
+
+/*Same example, using map function*/
+
+var a = ["kanthi", "keerthi", "sruthi"];
+var eleCheck=prompt('Enter any name!!!')
+a.map(function(a){
+    if(a==eleCheck)
+    console.log('valid name');
+    else
+    console.log('invalid');
+})
